c90b80f027fc8d0a13da82bfb2c1e1dc772af0bd
Anna sabbagh <afreenshabbagh@gmail.com>
2026-03-20 12:40:07 +0530

    fix: add vercel domain to CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:5173", "https://mern-thinkboard-eta.vercel.app"],
     credentials: true,
   }),
 );
